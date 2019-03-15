e394191314dc406b5afc005ba1558456df497874
Chris Kyrouac <chriskyrouac@gmail.com>
2019-03-14 18:20:40 -0700

    Completed assignment

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -14,31 +14,74 @@ void drive_robot(float lin_x, float ang_z)
         ROS_ERROR("Failed to call service DriveToTarget");
 }
 
-// This callback function continuously executes and reads the image data
+void move_left()
+{
+    drive_robot(0.0, 0.5);
+}
+
+void move_right()
+{
+    drive_robot(0.0, -0.5);
+}
+
+void move_forward()
+{
+    drive_robot(0.5, 0.0);
+}
+
+void stop()
+{
+    drive_robot(0, 0);
+}
+
 void process_image_callback(const sensor_msgs::Image img)
 {
-
     int white_pixel = 255;
+    int left_count = 0;
+    int right_count = 0;
+    int middle_count = 0;
+    int pixel_size = img.step / img.width;
+    
+    for (uint row = 0; row < img.height; row++) {
+        for (uint col = 0; col < img.width; col++) {
+            uint pixel_idx = (col * pixel_size) + (row * img.step);
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
+            if (img.data[pixel_idx] == white_pixel &&
+                img.data[pixel_idx + 1] == white_pixel &&
+                img.data[pixel_idx + 2] == white_pixel) {
+
+                uint third_width = img.width / 3;
+
+                if (col < third_width) {
+                    left_count++;
+                } else if (col > third_width && col < third_width * 2) {
+                    middle_count++;
+                } else if (col > third_width * 2 && col < img.width) {
+                    right_count++;
+                }
+            }
+        }
+    }
+
+    if (left_count != 0 || right_count != 0 || middle_count != 0) {
+        if (left_count > middle_count && left_count > right_count) {
+            move_left();
+        } else if (middle_count > left_count && middle_count > right_count) {
+            move_forward();
+        } else {
+            move_right();
+        }
+    } else {
+        stop();
+    }
 }
 
 int main(int argc, char** argv)
 {
-    // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "process_image");
     ros::NodeHandle n;
-
-    // Define a client service capable of requesting services from command_robot
     client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
-
-    // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
     ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
-
-    // Handle ROS communication events
     ros::spin();
 
     return 0;
